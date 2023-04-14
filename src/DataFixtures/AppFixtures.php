aebd2132534fcc3a73d3d4543785d1ff12eeca7d
Lek-San <leksan.webdev@gmail.com>
2023-04-14 14:33:16 +0200

    Update fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,27 +7,27 @@ use Doctrine\Persistence\ObjectManager;
 use App\Entity\Product;
 use App\Entity\Category;
 
-require_once 'vendor/autoload.php';
-
 class AppFixtures extends Fixture
 {
 
     public function load(ObjectManager $manager): void
     {
         for ($i = 1; $i <= 50; $i++) {
+
             $product = new Product();
         
             $product->setProductName('Produit ' . $i)
-                    ->setProductDesc('C\'est la description du produit ' . $i)
+                    ->setProductDesc('C\'est la description du produit n°' . $i)
                     ->setProductPrice(mt_rand(0.59, 99.99));
             
             $manager->persist($product);
         }
 
         for ($i = 1; $i < 10; $i++) {
+
             $category = new Category();
         
-            $category->setCategoryName('Categorie ' . $i);
+            $category->setCategoryName('Categotie ' . $i);
             
             $manager->persist($category);
         }
